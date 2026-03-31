a639486cd429aa0525beccb1f3684ca893686df6
Ramsés <ramsesaguirresan@gmail.com>
2026-03-31 13:25:29 +0200

    Implementado gestor de tareas simple

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
 """Menú de nuestra app"""
+from taskManager import TaskManager
+
+
+def printMenu():
+        print("\n --- Gestor de Tareas Inteligente---")
+        print("1. Añadir tarea.")
+        print("2. Listar tarea.")
+        print("3. Completar tarea.")
+        print("4. Borrar tarea.")
+        print("5. Salir")
+
+
+def main():
+    
+    manager = TaskManager() #inicializamos en una variable fuera del bucle (programación funcional)
+    
+    """Para crear una app que pregunte continuamente al usuario vamos a crear un bucle infinito hasta que el usuario pulse salir"""
+    while True:
+        
+        printMenu()
+        choice = input("Elige una opción: ")
+
+        match choice:
+            case "1":
+                description = input("Describa la tarea: ")
+                manager.add_task(description)
+            case "2":
+                manager.list_task()
+            case "3":
+                id = input("ID de la tarea a completar: ")
+                manager.complete_task(id)
+            case "4":
+                id = input ("ID de la tarea a eliminar: ")
+                manager.delete_task(id)
+            case "5":
+                print("Saliendo")
+                break     
+            case _: #en python este es igual que el default en js
+                print("Opción no válida. Selecciona otra.")
+            
+
+"""Esto hace que al iniciarse el archivo llamado main llame a la función main"""
+if __name__ == "__main__":
+    main()
--- taskManager.py
+++ taskManager.py
@@ -1,1 +1,51 @@
 """Funcionalidad de nuestra app"""
+
+class Task:
+    def __init__(self, id, descripcion, completada = False):
+        self.id = id
+        self.descripcion = descripcion
+        self.completada = completada
+    
+    def __str__(self): 
+        status = "✓" if self.completada else "✗" 
+        return f"[{status}] #{self.id}: {self.descripcion}"
+    
+class TaskManager:
+    def __init__(self):
+        """Definimos las variable que vamos a necesitar para iniciar una tarea"""
+        self.tasks = []  
+        self.next_id = 1
+    
+    def add_task(self, description):
+        """Para añadir tareas, crearemos una variable task que llamará a la clase Task()"""
+        task = Task(self.next_id, description)
+        """Añadiremos la tarea"""
+        self.tasks.append(task)
+        """Incrementamos el id"""
+        self.next_id += 1
+        print(f"Tarea añadida, {description}" )
+    
+    def list_task(self):
+        """Condición si no hay tareas"""
+        if not self.tasks:
+            print("No hay tareas pendientes")
+        else:
+            """Pero si hay tareas, se recorren y se muestran."""
+            for task in self.tasks:
+                print(task)
+    
+    def complete_task(self, id):
+        for task in self.tasks:
+            if task.id == id:
+                task.complete = True
+                print(f"Tarea completada: {task}")
+                return
+            print(f"Tarea no encontrada: #{id}")
+    
+    def delete_task(self, id):
+        for task in self.tasks:
+            if task.id == id:
+                self.tasks.remove(task)
+                print(f"Tarea eliminada: #{id}")
+                return
+            print(f"Tarea no encontrada: #{id}")
